b2a71a4331842284a372eeb0107bd11995cd3176
Greg <greg.renfro93@gmail.com>
2023-07-18 20:45:28 -0700

    Initial Commit

---
+++ shopaholics/lib/widgets/HomeAppBar.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeAppBar extends StatelessWidget {
+  const HomeAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(25),
+      child: Row(
+        children: [
+          Icon(
+            Icons.sort,
+            size: 30,
+            color: Color(0xFF4C53A5),
+          ),
+        ],
+      ),
+    );
+  }
+}
